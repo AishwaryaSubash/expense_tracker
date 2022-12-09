0130e88c1bce33786781bf13d6095380eeb0623f
Akash <67492990+Akash-Manikandan@users.noreply.github.com>
2022-12-09 19:57:26 +0530

    I like shiny things

--- lib/background.dart
+++ lib/background.dart
@@ -7,10 +7,10 @@ class Background extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Container(
-      height: 290,
+      height: 340,
       width: size.width,
       decoration: const BoxDecoration(
-        color: Color(0xff1d2a30),
+        color: Color(0xff1d2a31),
         borderRadius: BorderRadius.only(
           bottomLeft: Radius.elliptical(160, 50),
           bottomRight: Radius.elliptical(160, 50),
--- lib/profile.dart
+++ lib/profile.dart
@@ -1,45 +1,68 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/background.dart';
 import 'package:flutter_application_1/bottomnavigation.dart';
+import 'package:gap/gap.dart';
 
 class Profile extends StatelessWidget {
   const Profile({super.key});
 
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
     return Scaffold(
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
         iconTheme: const IconThemeData(
           color: Color(0xff1d2a31),
         ),
         title: const Text("Profile"),
         centerTitle: true,
-        backgroundColor: const Color(0xff1d2a30),
+        backgroundColor: Colors.transparent,
+        automaticallyImplyLeading: false,
       ),
       bottomNavigationBar: const BottomNavigation(
         page: "Profile",
       ),
       body: SingleChildScrollView(
-        child: Container(
-          height: 340,
-          child: Stack(
-            children: const [
-              Background(),
-              Positioned.fill(
-                bottom: -220,
-                child: Align(
-                  alignment: Alignment.center,
-                  child: CircleAvatar(
-                    backgroundImage: AssetImage(
-                      "assets/images/karthi.png",
+        child: Column(
+          children: [
+            SizedBox(
+              height: 400,
+              child: Stack(
+                children: const [
+                  Background(),
+                  Positioned.fill(
+                    bottom: -260,
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: CircleAvatar(
+                        backgroundImage: AssetImage(
+                          "assets/images/karthi.png",
+                        ),
+                        radius: 50,
+                      ),
                     ),
-                    radius: 50,
-                  ),
-                ),
-              )
-            ],
-          ),
+                  )
+                ],
+              ),
+            ),
+            const Text(
+              "Karthikeyan",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            const Gap(6),
+            const Text(
+              "@karthikeyan",
+              style: TextStyle(
+                fontSize: 14,
+              ),
+            ),
+            Column(
+              children: [],
+            )
+          ],
         ),
       ),
     );
