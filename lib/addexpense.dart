de7e1d68d3aa1cfee99f5898dcdba193b528c109
Aishwarya <riya129ash@gmail.com>
2022-12-11 20:40:52 +0530

    Try 100000

--- lib/addexpense.dart
+++ lib/addexpense.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/background.dart';
 import 'package:flutter_application_1/bottomnavigation.dart';
 import 'package:flutter_application_1/inputfields.dart';
+import 'package:gap/gap.dart';
 
 class AddExpense extends StatelessWidget {
   const AddExpense({super.key});
@@ -24,7 +25,7 @@ class AddExpense extends StatelessWidget {
       ),
       body: SingleChildScrollView(
         child: SizedBox(
-          height: size.height * 0.4,
+          height: 250,
           child: Stack(
             children: [
               const Background(),
@@ -37,7 +38,6 @@ class AddExpense extends StatelessWidget {
                       borderRadius: BorderRadius.circular(30),
                     ),
                     width: size.width * 0.8,
-                    height: size.height * 0.4,
                     child: const InputFields(),
                   ),
                 ),
