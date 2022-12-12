cb86b46794725b98891c50519bc08155d3b3e0c5
Aishwarya <riya129ash@gmail.com>
2022-12-12 12:26:20 +0530

    Changes reqd

--- lib/addexpense.dart
+++ lib/addexpense.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/background.dart';
 import 'package:flutter_application_1/bottomnavigation.dart';
 import 'package:flutter_application_1/inputfields.dart';
+
 import 'package:gap/gap.dart';
 
 class AddExpense extends StatelessWidget {
@@ -10,34 +11,63 @@ class AddExpense extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    // preferredSize: Size.fromHeight(50),
+
     return Scaffold(
-      appBar: AppBar(
-        iconTheme: const IconThemeData(
-          color: Color(0xff1d2a31),
-        ),
-        title: const Text("Add Expenses"),
-        centerTitle: true,
-        backgroundColor: const Color(0xff1d2a30),
-      ),
+      appBar: null,
+      // appBar: AppBar(
+      //   // iconTheme: const IconThemeData(
+      //   //   color: Color(0xff1d2a31),
+      //   // ),
+      //   title: const Text("Add Expenses"),
+      //   centerTitle: true,
+      //   // backgroundColor: transparent,
+      // ),
       bottomNavigationBar: const BottomNavigation(
         page: "Add",
       ),
       body: SingleChildScrollView(
-        child: SizedBox(
-          height: 250,
-          child: Stack(
+        child: Container(
+          height: size.height - 56,
+          child: Flex(
+            direction:
+                (size.width > size.height) ? Axis.horizontal : Axis.vertical,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              const Background(),
-              Positioned.fill(
-                child: Align(
-                  alignment: Alignment.center,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(30),
+              Expanded(
+                child: Center(
+                  child: SizedBox(
+                    width: 60,
+                    height: 60,
+                    child: Image.asset(
+                      "assets/images/rupee.png",
+                      width: 60,
+                      height: 60,
                     ),
-                    width: size.width * 0.8,
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: (size.width > size.height) ? 3 : 1,
+                child: SingleChildScrollView(
+                  child: Container(
+                    width: size.width,
+                    
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(50),
+                        topRight: Radius.circular(50),
+                      ),
+                      
+                      gradient: const LinearGradient(
+                        begin: Alignment(1.0, -1.0),
+                        end: Alignment(-1.0, 1.0),
+                        colors: [
+                          Color(0xff1d2a30),
+                          Color.fromARGB(255, 41, 76, 93),
+                        ],
+                      ),
+                    ),
                     child: const InputFields(),
                   ),
                 ),
@@ -49,3 +79,85 @@ class AddExpense extends StatelessWidget {
     );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+// import 'package:flutter/material.dart';
+// import 'package:flutter_application_1/background.dart';
+// import 'package:flutter_application_1/bottomnavigation.dart';
+// import 'package:flutter_application_1/inputfields.dart';
+// import 'package:gap/gap.dart';
+
+// class AddExpense extends StatelessWidget {
+//   const AddExpense({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     Size size = MediaQuery.of(context).size;
+//     // preferredSize: Size.fromHeight(50),
+//     return Scaffold(
+//       appBar: AppBar(
+//         iconTheme: const IconThemeData(
+//           color: Color(0xff1d2a31),
+//         ),
+//         title: const Text("Add Expenses"),
+//         centerTitle: true,
+//         backgroundColor: const Color(0xff1d2a30),
+//       ),
+//       bottomNavigationBar: const BottomNavigation(
+//         page: "Add",
+//       ),
+//       body: SingleChildScrollView(
+//         child: SizedBox(
+//           height: 250,
+//           child: Stack(
+//             children: [
+//               const Background(),
+//               Positioned.fill(
+//                 child: Align(
+//                   alignment: Alignment.center,
+//                   child: Container(
+//                     decoration: BoxDecoration(
+//                       color: Colors.white,
+//                       borderRadius: BorderRadius.circular(30),
+//                     ),
+//                     width: size.width * 0.8,
+//                     child: const InputFields(),
+//                   ),
+//                 ),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
+
+
+
+
+
+
+// class OpenPainter extends CustomPainter {
+//   @override
+//   void paint(Canvas canvas, Size size) {
+//     var paint1 = Paint()
+//       ..color = Color(0xff1d2a30)
+//       ..style = PaintingStyle.fill;
+//     canvas.drawCircle(Offset(90, -50), 300, paint1);
+//   }
+
+//   @override
+//   bool shouldRepaint(CustomPainter oldDelegate) => true;
+// }
--- lib/inputfields.dart
+++ lib/inputfields.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:intl/intl.dart';
@@ -24,7 +26,16 @@ class _InputFieldsState extends State<InputFields> {
     return Container(
       child: Column(
         children: [
-          Gap(20),
+          const Gap(20),
+          const Text(
+            "Add Your Expenses",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 17,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          const Gap(20),
           Container(
             width: size.width * 0.6,
             child: Column(
@@ -33,19 +44,31 @@ class _InputFieldsState extends State<InputFields> {
                   children: const [
                     Text(
                       "Description",
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 14,
+                      ),
                       textAlign: TextAlign.start,
                     ),
                   ],
                 ),
                 const TextField(
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.white,
+                  ),
                   decoration: InputDecoration(
-                    border: OutlineInputBorder(),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                      ),
+                    ),
                   ),
                 ),
               ],
             ),
           ),
-          Gap(10),
+          const Gap(10),
           Container(
             width: size.width * 0.6,
             child: Column(
@@ -54,29 +77,38 @@ class _InputFieldsState extends State<InputFields> {
                   children: const [
                     Text(
                       "Amount",
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.grey,
+                      ),
                       textAlign: TextAlign.start,
                     ),
                   ],
                 ),
                 const TextField(
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.white,
+                  ),
                   decoration: InputDecoration(
-                    border: OutlineInputBorder(),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                      ),
+                    ),
                   ),
                 ),
               ],
             ),
           ),
-          Gap(20),
+          const Gap(20),
           TextButton.icon(
             // autofocus: true,
             style: TextButton.styleFrom(
               padding: const EdgeInsets.all(20),
               backgroundColor: const Color(0xff1d2a30),
-              foregroundColor: Colors.grey,
+              foregroundColor: Colors.white,
             ),
-            // style: ButtonStyle(
-            //   foregroundColor: MaterialStateProperty.all<Color>(Colors.grey),
-            // ),
             onPressed: () {},
             label: const Text(
               'Add Expense',
@@ -88,6 +120,7 @@ class _InputFieldsState extends State<InputFields> {
               Icons.add,
             ),
           ),
+          const Gap(50),
         ],
       ),
     );
