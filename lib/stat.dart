20fbfe1649fdcb86735a795c06e3e26f611953cc
Akash <67492990+Akash-Manikandan@users.noreply.github.com>
2022-12-14 15:37:30 +0530

    OKK

--- lib/stat.dart
+++ lib/stat.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:http/http.dart' as http;
 import 'dart:async';
 import 'package:flutter/material.dart';
@@ -7,7 +8,9 @@ import 'package:flutter_application_1/chart.dart';
 import 'package:flutter_application_1/listcard.dart';
 import 'package:flutter_application_1/toggleswitching.dart';
 import 'package:gap/gap.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:intl/intl.dart';
 
 getStringValuesSF() async {
   SharedPreferences prefs = await SharedPreferences.getInstance();
@@ -25,7 +28,8 @@ class Stat extends StatefulWidget {
 
 class _StatState extends State<Stat> {
   dynamic stat = null;
-  double avg = 0;
+  dynamic data = null;
+  String avg = "";
   List<ChartData> chartData = <ChartData>[
     ChartData(
       x: 'Mon',
@@ -61,9 +65,13 @@ class _StatState extends State<Stat> {
     final response = await http.get(
       Uri.parse("https://expense-backend.vercel.app/stats/$res"),
     );
-    if (response.statusCode == 200) {
+    final expense = await http.get(
+      Uri.parse("https://expense-backend.vercel.app/user/$res"),
+    );
+    if (response.statusCode == 200 && expense.statusCode == 200) {
       setState(() {
         stat = jsonDecode(response.body);
+
         chartData = <ChartData>[
           ChartData(
             x: 'Mon',
@@ -94,19 +102,21 @@ class _StatState extends State<Stat> {
             y: stat[0]["quota"][0],
           ),
         ];
-        avg = double.parse(((stat[0]["quota"].reduce((a, b) => a + b)) /
-                stat[0]["quota"].length)
-            .toStringAsFixed(2));
+        avg = formatter!.format(((stat[0]["quota"].reduce((a, b) => a + b)) /
+            stat[0]["quota"].length));
+        data = jsonDecode(expense.body);
       });
     } else {
       throw Exception('Failed to load statistics');
     }
   }
 
+  NumberFormat? formatter;
   @override
   void initState() {
     super.initState();
-
+    initializeDateFormatting("in");
+    formatter = NumberFormat('#,##,000');
     fetchAlbum();
   }
 
@@ -163,140 +173,106 @@ class _StatState extends State<Stat> {
       bottomNavigationBar: const BottomNavigation(
         page: "Stat",
       ),
-      body: RefreshIndicator(
-        onRefresh: () {
-          return Future.delayed(const Duration(seconds: 1), () {
-            setState(
-              () {
-                refreshPull();
+      body: (data != null)
+          ? RefreshIndicator(
+              onRefresh: () {
+                return Future.delayed(const Duration(seconds: 1), () {
+                  setState(
+                    () {
+                      refreshPull();
+                    },
+                  );
+                });
               },
-            );
-          });
-        },
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(15),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  "Expenses Average",
-                  style: TextStyle(
-                    color: Colors.grey,
-                    fontSize: 17,
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.all(15),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        "Expenses Average",
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 17,
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            "₹$avg",
+                            style: const TextStyle(
+                              fontSize: 25,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          const Gap(20),
+                          const Icon(
+                            Icons.arrow_drop_up_rounded,
+                            size: 35,
+                          ),
+                          const Text(
+                            "20%",
+                            style: TextStyle(
+                              fontSize: 14,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const Gap(20),
+                      Chart(
+                        chartData: chartData,
+                        stat: stat,
+                      ),
+                      const Gap(25),
+                      const Toggler(),
+                      const Gap(20),
+                      const Text(
+                        "Expenses",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      const Gap(20),
+                      ...List.from(
+                        data["expense"]
+                            .map(
+                              (i) => ListCard(
+                                image: i["description"],
+                                name: i["description"],
+                                price: (i["amount"].toString().length > 3)
+                                    ? "-₹${formatter?.format(i["amount"])}"
+                                    : "-₹${i["amount"]}",
+                                time: DateFormat.jm().format(
+                                  DateTime.parse(
+                                    i["date"],
+                                  ).toLocal(),
+                                ),
+                                color: 0xffebf9ff,
+                                date: DateFormat.E().format(
+                                  DateTime.parse(
+                                    i["date"],
+                                  ).toLocal(),
+                                ),
+                              ),
+                            )
+                            .toList()
+                            .reversed,
+                      ),
+                      const Gap(10),
+                    ],
                   ),
                 ),
-                Row(
-                  children: [
-                    Text(
-                      "₹$avg",
-                      style: const TextStyle(
-                        fontSize: 25,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                    const Gap(20),
-                    const Icon(
-                      Icons.arrow_drop_down_circle_rounded,
-                      size: 15,
-                    ),
-                    const Text(
-                      "20%",
-                      style: TextStyle(
-                        fontSize: 14,
-                      ),
-                    ),
-                  ],
-                ),
-                const Gap(20),
-                Chart(chartData: chartData, stat: stat),
-                const Gap(25),
-                const Toggler(),
-                const Gap(20),
-                const Text(
-                  "Categories",
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-                const Gap(20),
-                const ListCard(
-                    image: "hackerrank",
-                    name: "Hackerrank",
-                    price: "-₹50.00",
-                    time: "16:30 PM",
-                    color: 0xffebf9ff,
-                    date: ""),
-                const ListCard(
-                    image: "hackerrank",
-                    name: "Hackerrank",
-                    price: "-₹50.00",
-                    time: "16:30 PM",
-                    color: 0xffebf9ff,
-                    date: ""),
-                const ListCard(
-                    image: "hackerrank",
-                    name: "Hackerrank",
-                    price: "-₹50.00",
-                    time: "16:30 PM",
-                    color: 0xffebf9ff,
-                    date: ""),
-                const ListCard(
-                    image: "hackerrank",
-                    name: "Hackerrank",
-                    price: "-₹50.00",
-                    time: "16:30 PM",
-                    color: 0xffebf9ff,
-                    date: ""),
-                const ListCard(
-                    image: "hackerrank",
-                    name: "Hackerrank",
-                    price: "-₹50.00",
-                    time: "16:30 PM",
-                    color: 0xffebf9ff,
-                    date: ""),
-                const ListCard(
-                    image: "hackerrank",
-                    name: "Hackerrank",
-                    price: "-₹50.00",
-                    time: "16:30 PM",
-                    color: 0xffebf9ff,
-                    date: ""),
-                const ListCard(
-                    image: "hackerrank",
-                    name: "Hackerrank",
-                    price: "-₹50.00",
-                    time: "16:30 PM",
-                    color: 0xffebf9ff,
-                    date: ""),
-                const ListCard(
-                    image: "hackerrank",
-                    name: "Hackerrank",
-                    price: "-₹50.00",
-                    time: "16:30 PM",
-                    color: 0xffebf9ff,
-                    date: ""),
-                const ListCard(
-                    image: "hackerrank",
-                    name: "Hackerrank",
-                    price: "-₹50.00",
-                    time: "16:30 PM",
-                    color: 0xffebf9ff,
-                    date: ""),
-                const ListCard(
-                    image: "hackerrank",
-                    name: "Hackerrank",
-                    price: "-₹50.00",
-                    time: "16:30 PM",
-                    color: 0xffebf9ff,
-                    date: ""),
-                const Gap(10),
-              ],
+              ),
+            )
+          : const Padding(
+              padding: EdgeInsets.all(20.0),
+              child: SpinKitCircle(
+                size: 100,
+                color: Color(0xff1d2a31),
+              ),
             ),
-          ),
-        ),
-      ),
     );
   }
 }
